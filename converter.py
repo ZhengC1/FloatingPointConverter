d38e5607f37d081df02e7714a29c1ce710a3526e
ZhengC1 <zhengchun90@gmail.com>
2016-02-09 00:23:34 -0500

    converted hex to binary while keeping leading zeros

--- converter.py
+++ converter.py
@@ -1,11 +1,17 @@
 #! usr/bin/python
 
 def convert():
-    number = "10101010101010101010101010101011"
-    print number[:1]
 
-    print number[1:9]
-    print int(number[1:9], 2)
-    print number[9:31]
+    hex_number = raw_input("whats your number in hex?: ")
+
+    h_size = len(hex_number) * 4
+
+    binary_number = ( bin(int(hex_number, 16))[2:] ).zfill(h_size)
+
+    print binary_number[:1]
+
+    print binary_number[1:9]
+    print int(binary_number[1:9], 2)
+    print binary_number[9:31]
 
 convert()
