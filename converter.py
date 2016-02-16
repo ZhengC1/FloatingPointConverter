22ea4deb21e4ec5207622a54791a35dc1ae78009
ZhengC1 <zhengc1@appstate.edu>
2016-02-16 00:19:26 -0500

    take in some input

--- converter.py
+++ converter.py
@@ -1,17 +1,28 @@
 #! usr/bin/python
 
-def convert():
+class floating_point(object):
 
-    hex_number = raw_input("whats your number in hex?: ")
+    def __init__(self):
+        print "hello, this is a 16 bit floating point converter,
+        it will convert from binary to decimal."
+        cat = raw_input("what is are your binary digits?: ")
+        first_bit = cat[1:]
+        print first_bit
+        charateristic = cat[2:10]
+        print charateristic
 
-    h_size = len(hex_number) * 4
+    def convert():
+        hex_number = raw_input("whats your number in hex?: ")
 
-    binary_number = ( bin(int(hex_number, 16))[2:] ).zfill(h_size)
+        h_size = len(hex_number) * 4
 
-    print binary_number[:1]
+        binary_number = ( bin(int(hex_number, 16))[2:] ).zfill(h_size)
 
-    print binary_number[1:9]
-    print int(binary_number[1:9], 2)
-    print binary_number[9:31]
+        print binary_number[:1]
 
-convert()
+        print binary_number[1:9]
+        print int(binary_number[1:9], 2)
+        print binary_number[9:31]
+
+squirrel = new floating_point()
+
