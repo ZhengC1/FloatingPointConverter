3f2a803fe33a2500bdfe99aaa01ca1e48de7d1cf
ZhengC1 <zhengc1@appstate.edu>
2016-02-17 19:56:50 -0500

    seperated the string up

--- converter.py
+++ converter.py
@@ -1,28 +1,26 @@
 #! usr/bin/python
 
-class floating_point(object):
+#this is the init function that will take in the input from the command line
+def converting():
+    print "hello, this is a 16 bit floating point converter it will convert from binary to decimal."
+    #cat = raw_input("what is are your binary digits?: ")
+    cat = "123456789ABCDEFG"
+    print "first digit"
+    first_bit = cat[:1:]
+    print first_bit
+    print "charateristic"
+    charateristic = cat[1:6]
+    print charateristic
+    print "mantissa"
+    mantissa = cat[6:16]
+    print mantissa
 
-    def __init__(self):
-        print "hello, this is a 16 bit floating point converter,
-        it will convert from binary to decimal."
-        cat = raw_input("what is are your binary digits?: ")
-        first_bit = cat[1:]
-        print first_bit
-        charateristic = cat[2:10]
-        print charateristic
+    result = []
 
-    def convert():
-        hex_number = raw_input("whats your number in hex?: ")
+    if(first_bit == "1"):
+        result.append("-")
+    result.append(
 
-        h_size = len(hex_number) * 4
 
-        binary_number = ( bin(int(hex_number, 16))[2:] ).zfill(h_size)
 
-        print binary_number[:1]
-
-        print binary_number[1:9]
-        print int(binary_number[1:9], 2)
-        print binary_number[9:31]
-
-squirrel = new floating_point()
-
+converting()
